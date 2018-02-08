d984f28e0c53af43d6c6d3d5834a159af919fc06
熊向明 <xiongxiangming@xiongxiangmingdeMacBook-Pro.local>
2018-02-08 12:23:47 +0800

    add comment for run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+#更新redis-doc及redis-io源码
 cd /redis/redis-doc
-#git pull
+git pull
 cd /redis/redis-io
-#git pull
+git pull
+#启动redis服务
 /usr/local/bin/redis-server & 
+#rackup启动文档服务
 REDIS_DOC=/redis/redis-doc /usr/local/bin/rackup -o 0.0.0.0 &
